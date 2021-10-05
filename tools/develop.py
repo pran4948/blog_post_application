d02d62493342ea0ad9b68be46818f97323055a34
SabrinaKhatoon <sabrinakhatoon30@gmail.com>
2021-10-05 23:16:55 +0530

    fixed a bug

---
+++ tools/develop.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+graph = {'A': ['B', 'D', 'G', 'I'], 
+         'B': ['D', 'C'], 
+         'C' : ['A', 'E'], 
+         'D' : ['F','C'], 
+         'E' : ['F', 'J'], 
+         'F' : ['H','C'], 
+         'G':['F','K'], 
+         'H':['E', 'K', 'L'], 
+         'I':['S'], 
+         'J':['S'], 
+         'K':[], 
+         'L':['J','F'], 
+         'S':['K'] }
+
+print(graph)
+
+
+# In[8]:
+
+
+def dfs(graph, source):
+    S = list()
+    visited_vertices = list()
+    S.append(source)
+    visited_vertices.append(source)
+    while S:
+        vertex = S.pop()
+        print(vertex, end="-->")
+        for u in graph[vertex]:
+            if u not in visited_vertices:
+                S.append(u)
+                visited_vertices.append(u)
+ 
+print("DFS traversal of graph with source 'A' is:")
+dfs(graph, 'A')
+
+
+# In[ ]:
+
+
+
+

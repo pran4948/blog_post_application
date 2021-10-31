6722e7bef59986118afff089765f5dca4a66040f
Harsh-Bhabar <55344824+Harsh-Bhabar@users.noreply.github.com>
2021-10-31 11:46:25 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+# comment - Harsh Bhabar
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
